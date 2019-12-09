5fe9fff12f3abac5ad10fbfcbc24ea79a482b8e9
Yannick Lang <layaxx@gmx.de>
2019-12-09 23:56:41 +0100

    fixed bug with custum color selection

--- config/selection.js
+++ config/selection.js
@@ -172,6 +172,10 @@ var LightenColor = function (color, percent) {
     return (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
 };
 
+function bugfix() {
+    document.getElementById("color_costum").value = '#444444';
+}
+
 
 function changeColorCostum() {
     var newColor = document.getElementById("color_costum").value;
